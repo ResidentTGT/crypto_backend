2c34327017a774cd4e58a56cdbd8aa03088c0bdd
Anatoly Romanov <tromanov95@mail.ru>
2024-08-30 09:59:34 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ImATeapotException } from '@nestjs/common';
 const whitelist = ['*'];
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         logger: ['log', 'fatal', 'error', 'warn', 'debug', 'verbose'],
-        cors: {
-            methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
-            origin: function (origin, callback) {
-                if (!origin) {
-                    callback(null, true);
-                    return;
-                }
-                if (
-                    whitelist.includes(origin) || // Checks your whitelist
-                    !!origin.match(/yourdomain\.com$/) // Overall check for your domain
-                ) {
-                    console.log('allowed cors for:', origin);
-                    callback(null, true);
-                } else {
-                    console.log('blocked cors for:', origin);
-                    callback(new ImATeapotException('Not allowed by CORS'), false);
-                }
-            },
-        },
+    });
+    app.enableCors({
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+        origin: whitelist,
     });
     await app.listen(3001);
 }
