1403d6fec4f7f393eb0d4c01f7afe8d3c876e904
Anatoly Romanov <tromanov95@mail.ru>
2024-08-30 10:03:54 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,7 @@ async function bootstrap() {
         logger: ['log', 'fatal', 'error', 'warn', 'debug', 'verbose'],
     });
     app.enableCors({
-        origin: function (origin, callback) {
-            console.log(origin);
-            if (whitelist.indexOf(origin) !== -1) {
-                console.log('allowed cors for:', origin);
-                callback(null, true);
-            } else {
-                console.log('blocked cors for:', origin);
-                callback(new Error('Not allowed by CORS'));
-            }
-        },
+        origin: true,
         allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
         methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
         credentials: true,
