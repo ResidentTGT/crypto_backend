e00e759c7d61591fd8f649850bb3b3d9a454104b
Anatoly Romanov <tromanov95@mail.ru>
2024-08-30 10:18:24 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,16 @@ const whitelist = ['https://crypto-resident.web.app/'];
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
-        logger: ['log', 'fatal', 'error', 'warn', 'debug', 'verbose'],
-    });
-    app.enableCors({
-        origin: function (origin, callback) {
-            if (!origin || whitelist.indexOf(origin) !== -1) {
+        cors: {
+            origin: function (origin, callback) {
                 callback(null, true);
-            } else {
-                callback(new Error('Not allowed by CORS'));
-            }
+            },
+            allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+            methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+            credentials: true,
         },
-        allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-        methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
-        credentials: true,
     });
+
     await app.listen(3001);
 }
 bootstrap();
