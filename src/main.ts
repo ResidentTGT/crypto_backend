69ea54c294374f3c11277e90669531603ab89fce
Anatoly Romanov <tromanov95@mail.ru>
2024-08-31 21:58:53 +0300

    ssl

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const whitelist = ['https://crypto-resident.web.app/'];
+import * as fs from 'fs';
 
 async function bootstrap() {
+    const keyFile = fs.readFileSync(__dirname + '/../ssl/cert.key');
+    const certFile = fs.readFileSync(__dirname + '/../ssl/cert.crt');
     const app = await NestFactory.create(AppModule, {
         cors: {
             origin: '*',
             allowedHeaders: '*',
             methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
             credentials: true,
+        },
+        httpsOptions: {
+            key: keyFile,
+            cert: certFile,
         },
     });
 
